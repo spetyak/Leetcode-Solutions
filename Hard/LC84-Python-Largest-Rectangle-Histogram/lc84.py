47a8c2cb95b17700554a674ca4dc00c883364430
spetyak <smpetyak@gmail.com>
2024-10-20 15:04:06 -0400

    added LC84

--- Hard/LC84-Python-Largest-Rectangle-Histogram/lc84.py
+++ Hard/LC84-Python-Largest-Rectangle-Histogram/lc84.py
@@ -2,8 +2,8 @@
 """
 Author: spetyak
 
-Runtime: Beats --.--% of Python submissions
-Memory: Beats --.--% of Python submissions
+Runtime: Beats 99.80% of Python submissions
+Memory: Beats 12.40% of Python submissions
 
 """
 
@@ -21,15 +21,58 @@ class Solution(object):
             int: the area of the largest rectangle in the histogram
         """
         
+        stack = []  # a increasing monotonic stack of the heights of the histogram 
+        maxArea = 0 # the maximum rectangle area found so far
         
+        for i in range(len(heights)):
+            
+            # if the stack is not empty and the current height is less than the top of the stack
+            if len(stack) > 0 and heights[i] < stack[-1][1]:
+            
+                # remove elements larger than the current height and check their areas as they are popped
+                while (len(stack) > 0 and heights[i] < stack[-1][1]):
+                    
+                    poppedHeight = stack.pop()
+                    width = i - poppedHeight[0]
+                    height = poppedHeight[1]
+                    
+                    currentArea = width * height # determine the area of the popped rectangle
+                    
+                    # update max area
+                    if currentArea > maxArea:
+                        
+                        maxArea = currentArea
+                        
+                stack.append((poppedHeight[0], heights[i])) # add new height to stack, able to form rectangle with last popped element
+                
+            else:
+            
+                stack.append((i, heights[i])) 
+                
+
         
-        return 0
+        # pop remaining elements from stack checking the are of the rectangles than can form
+        end = len(heights)
+        while len(stack) > 0:
+            
+            poppedHeight = stack.pop()
+            width = end - poppedHeight[0]
+            height = poppedHeight[1]
+            
+            currentArea = width * height # determine the area of the popped rectangle
+            
+            # update max area
+            if (currentArea > maxArea):
+                
+                maxArea = currentArea
+        
+        return maxArea
     
 def main():
 
     sol = Solution()
     
-    heights = [2,1,2] # 3
+    heights = [2,1,2] 
     
     print(f"Output: {sol.largestRectangleArea(heights)}")
 
